998f7e5bd9cdc2f8157a6cf36ee87de82e432afc
Nickhodem <nickhodem@gmail.com>
2015-07-05 20:01:12 +0200

    app for uploading

---
+++ swop/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import patterns, url, include
+from swop import views
+from django.conf import settings
+from django.conf.urls.static import static
+from swop.views import ProfileDetailView, ProfileImageView
+
+urlpatterns = patterns('',
+    #url(r'^$', ProfileImageIndexView.as_view(), name='home'),
+    url(r'^$', views.swopengine, name='swop'),
+
+    url(r'^upload/', ProfileImageView.as_view(), name='profile_image_upload'),
+    url(r'^uploaded/(?P<pk>\d+)/$', ProfileDetailView.as_view(),
+        name='profile_image'),
+
+
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+'''
+urlpatterns = patterns('swop.views',
+    url(r'^list/$', 'list', name='list'),
+    url(r'^$', 'swopengine', name='swop'),
+
+)
+
+
+urlpatterns=patterns('',
+                     url(r'^$', views.swopengine, name='index')
+                     )
+'''
---
+++ swop/views.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.views.generic import FormView, DetailView, ListView
+from swop.forms import ProfileImageForm
+from swop.models import ProfileImage
+
+def swopengine(request):
+    return HttpResponse('Lets <a href="/swop/upload/">upload</a> your files')
+def uploaded():
+    #odpowiedz na poprawne zarejestrowanie sie
+    return HttpResponse('Thanks for uploading </br> powrot do strony glownej </br> <a href="http://127.0.0.1:8000/">powrot</a>')
+
+
+
+
+class ProfileImageView(FormView):
+    template_name = '/Documents/Django/fabric/templates/swop/profile_image_form.html'
+    form_class = ProfileImageForm
+
+    def form_valid(self, form):
+        profile_image=ProfileImage(image=self.get_form_kwargs().get('files')['image'])
+        profile_image.save()
+        self.id=profile_image.id
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        #return HttpResponse('Thanks for uploading </br> powrot do strony glownej </br> <a href="http://127.0.0.1:8000/">powrot</a>')
+
+        return reverse('profile_image', kwargs={'pk':self.id})
+
+
+class ProfileDetailView(DetailView):
+    model=ProfileImage
+    template_name = '/Documents/Django/fabric/templates/swop/profile_image.html'
+    context_object_name = 'image'
+class ProfileImageIndexView(ListView):
+    model=ProfileImage
+    template_name = '/Documents/Django/fabric/templates/swop/profile_image_view.html'
+    context_object_name = 'images'
+    queryset = ProfileImage.objects.all()
+
+
+
+
+'''
+
+def list(request):
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            #file is saved
+            form.save()
+            return uploaded(request)
+    else:
+        form=DocumentForm()
+    return render(request,'/Documents/Django/fabric/templates/swop/list.html', {'form': form})
+
+
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('swop.views.list'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render_to_response(
+        '/Documents/Django/fabric/templates/swop/list.html',
+        {'documents': documents, 'form': form},
+        context_instance=RequestContext(request)
+    )
+'''
